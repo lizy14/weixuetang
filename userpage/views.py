fb8a2d424fba92c4a4c4430df4ce2bac5e83da62
b-liu14 <b_liu14@163.com>
2016-12-01 18:22:19 +0800

    save point

--- userpage/views.py
+++ userpage/views.py
@@ -1,30 +1,1 @@
-from django.shortcuts import render
-from codex.baseview import APIView
-from codex.baseerror import *
-from wechat.models import Student
-import urllib.request
-
-class UserBind(APIView):
-
-    def validate_user(self):
-        data = {
-            'username': self.input['student_id'],
-            'password': self.input['password']
-        }
-        res = eval(urllib.request.urlopen(urllib.request.Request(
-			url='https://its.tsinghua.edu.cn/loginAjax',
-			data=urllib.parse.urlencode(data).encode('utf-8'),
-			method='POST')).read().decode())
-        if res['code'] is not 0:
-            raise ValidateError(res['msg'])
-
-    def get(self):
-        self.check_input('openid')
-        return Student.get_by_openid(self.input['openid']).student_id
-
-    def post(self):
-        self.check_input('openid', 'student_id', 'password')
-        user = Student.get_by_openid(self.input['openid'])
-        self.validate_user()
-        user.student_id = self.input['student_id']
-        user.save()
+from django.shortcuts import renderfrom codex.baseview import APIViewfrom codex.baseerror import *from wechat.models import Studentimport urllib.requestclass UserBind(APIView):    def validate_user(self):        data = {            'username': self.input['student_id'],            'password': self.input['password']        }        res = eval(urllib.request.urlopen(urllib.request.Request(            url='https://its.tsinghua.edu.cn/loginAjax',            data=urllib.parse.urlencode(data).encode('utf-8'),            method='POST')).read().decode())        if res['code'] is not 0:            raise ValidateError(res['msg'])    def get(self):        self.check_input('openid')        return Student.get_by_openid(self.input['openid']).student_id    def post(self):        self.check_input('openid', 'student_id', 'password')        user = Student.get_by_openid(self.input['openid'])        self.validate_user()        user.student_id = self.input['student_id']        user.save()
