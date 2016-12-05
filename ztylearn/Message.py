eea77715de5a8a84212b07571d6bba1bd97c3ffe
Li Zhaoyang <lizy14@yeah.net>
2016-12-05 21:04:41 +0800

    /api/notice/*

--- ztylearn/Message.py
+++ ztylearn/Message.py
@@ -1,10 +1,11 @@
 class Message:
-    def __init__(self, user, id, title, date, detail):
+    def __init__(self, user, id, title, date, detail, author):
         self.title = title.strip()
         self.date = date
         self.user = user
         self.id = id
         self.detail = detail
+        self.author = author
 
 
     @property
