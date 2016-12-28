2e7997f11baccaa107366ab3c0bd1656f536c8fa
Jmq14 <jmq14@mails.tsinghua.edu.cn>
2016-12-29 01:01:13 +0800

    calendar: data parsers for week view

--- static/js/base.js
+++ static/js/base.js
@@ -63,52 +63,6 @@ window.getJSON = function(url, payload, callback){
     )
 }
 
-window.schedule = function (items, num_dates, month) {
-    new_items = new Array(num_dates);
-    for (var i = 0; i < num_dates; i++) {
-        new_items[i] = new Array();
-    }
-    if (!items) return new_items;
-    items.forEach(function(i) {
-        if (i.begin) { // curriculum
-            d = parseDate(i.begin);
-            if (d.getMonth() != month) return;
-            index = d.getDate() - 1;
-
-            i['start_time'] = {'hour': d.getHours(), 'min': d.getMinutes()};
-            e = parseDate(i.end);
-            i['end_time'] = {'hour': e.getHours(), 'min': e.getMinutes()}
-        }
-        else if (i.date) { // global events
-            d = parseDate(i.date);
-            if (d.getMonth() != month) return;
-            index = d.getDate() - 1;
-        }
-        else if (i.end_time) {  // homework
-            d = parseDate(i.end_time);
-            if (d.getMonth() != month) return;
-            index = d.getDate() - 1;
-        }
-        else return;
-
-        new_items[index].push(i);
-    });
-    return new_items;
-}
-
-window.calculate_margin = function(day) {
-    fst_day = new Date(day.getFullYear(), day.getMonth(), 1).getDay();
-    if (fst_day == 0) before = 6;
-    else before = fst_day - 1;
-    lst_day = new Date(day.getFullYear(), day.getMonth()+1, 0).getDay()
-    if (lst_day == 0) after = 0;
-    else after = 7 - lst_day;
-    return {
-        before:  before,
-        after: after
-    }
-}
-
 window.postForm = function(url, payload, callback){
     payload = $.extend(payload, window.urlParam);
     $.post(
@@ -129,6 +83,99 @@ window.postForm = function(url, payload, callback){
     )
 }
 
+
+// followings are for calendar
+window.is_same_week = function(a, b) {
+    day = b.getUTCDay();
+    if (a >= b) {
+        if ((a - b)/1000/86400 < 7 - day) return true;
+        else return false;
+    }
+    else (a < b){
+        if ((b - a)/1000/86400 < day + 1) return true;
+        else return false;
+    }
+    else return true;
+}
+    
+
+window.schedule = function (items, num_dates, focus_day) {
+    new_items = new Array(num_dates);
+    for (var i = 0; i < num_dates; i++) {
+        new_items[i] = new Array();
+    }
+    if (!items) return new_items;
+    
+    if (num_dates == 7) { // Schedule for week view
+        items.forEach(function(i) {
+            if (i.begin) { // curriculum
+                d = parseDate(i.begin);
+                if (!is_same_week(d, focus_day)) return;
+                index = d.getUTCDay();
+
+                i['start_time'] = {'hour': d.getHours(), 'min': d.getMinutes()};
+                e = parseDate(i.end);
+                i['end_time'] = {'hour': e.getHours(), 'min': e.getMinutes()}
+            }
+            else if (i.date) { // global events
+                d = parseDate(i.date);
+                if (!is_same_week(d, focus_day)) return;
+                index = d.getUTCDay();
+            }
+            else if (i.end_time) {  // homework
+                d = parseDate(i.end_time);
+                if (!is_same_week(d, focus_day)) return;
+                index = d.getUTCDay();
+            }
+            else return;
+
+            new_items[index].push(i);
+        });
+    }
+    else {  // Schedule for month view
+        month = focus_day.getMonth();
+        items.forEach(function(i) {
+            if (i.begin) { // curriculum
+                d = parseDate(i.begin);
+                if (d.getMonth() != month) return;
+                index = d.getDate() - 1;
+
+                i['start_time'] = {'hour': d.getHours(), 'min': d.getMinutes()};
+                e = parseDate(i.end);
+                i['end_time'] = {'hour': e.getHours(), 'min': e.getMinutes()}
+            }
+            else if (i.date) { // global events
+                d = parseDate(i.date);
+                if (d.getMonth() != month) return;
+                index = d.getDate() - 1;
+            }
+            else if (i.end_time) {  // homework
+                d = parseDate(i.end_time);
+                if (d.getMonth() != month) return;
+                index = d.getDate() - 1;
+            }
+            else return;
+
+            new_items[index].push(i);
+        });
+    }
+    
+    return new_items;
+}
+
+window.calculate_margin = function(day) {
+    fst_day = new Date(day.getFullYear(), day.getMonth(), 1).getDay();
+    if (fst_day == 0) before = 6;
+    else before = fst_day - 1;
+    lst_day = new Date(day.getFullYear(), day.getMonth()+1, 0).getDay()
+    if (lst_day == 0) after = 0;
+    else after = 7 - lst_day;
+    return {
+        before:  before,
+        after: after
+    }
+}
+
 window.produce_course_block = function (data) {
     if (!data) return [];
     data.forEach(function(i){
@@ -140,20 +187,24 @@ window.produce_course_block = function (data) {
     });
 }
 
-window.month_range = function(date) {
-    year = date.getFullYear();
-    month = date.getMonth();
+window.month_range = function(data) {
+    year = data.getFullYear();
+    month = data.getMonth();
     s = new Date(year, month, 1, 8).toISOString().substring(0,10);
     e = new Date(year, month+1, 0, 8).toISOString().substring(0,10);
     return {'start': s, 'end': e}
 }
 
 window.week_range = function(data) {
-    // TODO
-    year = date.getYear() + 1900;
-    day = date.getMonth();
-    s = new Date(year, month, 1, 8).toISOString().substring(0,10);
-    e = new Date(year, month+1, 0, 8).toISOString().substring(0,10);
+    year = data.getFullYear();
+    month = data.getMonth()
+    date = data.getDate()
+    day = data.getDay();
+    if (day == 0) day = 6;
+    else day = day - 1;
+
+    s = new Date(year, month, date-day, 8).toISOString().substring(0,10);
+    e = new Date(year, month, date-day+7, 8).toISOString().substring(0,10);
     return {'start': s, 'end': e}
 }
 
