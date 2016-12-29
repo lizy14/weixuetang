8ece1b979fdee0466be0586b54167daca451f402
Jmq14 <jmq14@mails.tsinghua.edu.cn>
2016-12-29 08:52:04 +0800

    calendar: fix bug

--- WeLearn/views.py
+++ WeLearn/views.py
@@ -21,7 +21,7 @@ class StaticFileView(BaseView): # pragma: no cover
     def do_dispatch(self, *args, **kwargs):
         if not settings.DEBUG:
             self.logger.warn('Please use nginx/apache to serve static files in production!')
-            raise Http404()
+            # raise Http404()
         rpath = self.request.path.replace('..', '.').strip('/')
         if '__' in rpath:
             raise Http404('Could not access private static file: ' + self.request.path)
